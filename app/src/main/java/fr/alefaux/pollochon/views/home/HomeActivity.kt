dad76fe4b6e925fc7e25e51613f7991f42706e90
Axel Lefaux <axel.lefaux@outlook.fr>
2020-05-06 13:36:06 +0200

    Add navigation graph

---
+++ app/src/main/java/fr/alefaux/pollochon/views/home/HomeActivity.kt
@@ -0,0 +1,6 @@
+package fr.alefaux.pollochon.views.home
+
+import androidx.appcompat.app.AppCompatActivity
+import fr.alefaux.pollochon.R
+
+class HomeActivity : AppCompatActivity(R.layout.activity_home)
