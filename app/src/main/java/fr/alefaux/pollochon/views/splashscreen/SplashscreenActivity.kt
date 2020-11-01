5b9049a8f522a3e594c6f3445f9f6bdc9dbc545a
Axel Lefaux <axel.lefaux@outlook.fr>
2020-11-01 13:31:46 +0100

    Change view to use compose

--- app/src/main/java/fr/alefaux/pollochon/views/splashscreen/SplashscreenActivity.kt
+++ app/src/main/java/fr/alefaux/pollochon/views/splashscreen/SplashscreenActivity.kt
@@ -3,20 +3,38 @@ package fr.alefaux.pollochon.views.splashscreen
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.ConstraintLayout
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.lifecycleScope
-import fr.alefaux.pollochon.databinding.ActivitySplashscreenBinding
+import androidx.ui.tooling.preview.Preview
+import fr.alefaux.pollochon.R
+import fr.alefaux.pollochon.theming.PollochonTheme
+import fr.alefaux.pollochon.theming.splashscreenBackgroundColor
 import fr.alefaux.pollochon.views.home.HomeActivity
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class SplashscreenActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySplashscreenBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySplashscreenBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+
+        setContent {
+            SplashscreenContent()
+        }
 
         lifecycleScope.launch {
             delay(3000)
@@ -26,4 +44,38 @@ class SplashscreenActivity : AppCompatActivity() {
         }
     }
 
+}
+
+@Composable
+fun SplashscreenContent() {
+    PollochonTheme {
+        ConstraintLayout(
+            modifier = Modifier.fillMaxSize().then(
+                Modifier.background(splashscreenBackgroundColor)
+            )
+        ) {
+            val (logoImage, titleText) = createRefs()
+            Image(vectorResource(R.drawable.ic_logo),
+                modifier = Modifier.constrainAs(logoImage) {
+                    centerTo(parent)
+                })
+            Text(
+                stringResource(id = R.string.app_name),
+                style = TextStyle(
+                    color = Color.White,
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold
+                ),
+                modifier = Modifier.constrainAs(titleText) {
+                    top.linkTo(parent.top, margin = 24.dp)
+                    centerHorizontallyTo(parent)
+                })
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SplashscreenContent_Preview() {
+    SplashscreenContent()
 }
